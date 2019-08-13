e84eb4fa101c8e912a0e31d8c387a0a32213582d
Loic Masson <loic.masson@blueprintgenetics.com>
2019-08-13 13:39:39 +0300

    Search and images for the card

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
+import { CardList } from "./components/card-list/card-list.component";
+import { SearchBox } from "./components/search-box/search-box.component";
+
 import "./App.css";
 
 class App extends Component {
@@ -7,29 +9,33 @@ class App extends Component {
     super();
 
     this.state = {
-      monsters: [
-        {
-          name: "Frankenstein",
-          id: "fra"
-        },
-        {
-          name: "Dracula",
-          id: "dra"
-        },
-        {
-          name: "Zombie",
-          id: "zom"
-        }
-      ]
+      monsters: [],
+      searchField: ""
     };
   }
 
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then(response => response.json())
+      .then(users => this.setState({ monsters: users }));
+  }
+
+  handleChange = e => {
+    this.setState({ searchField: e.target.value });
+  };
+
   render() {
+    const { monsters, searchField } = this.state;
+    const filteredMonsters = monsters.filter(monster =>
+      monster.name.toLowerCase().includes(searchField.toLowerCase())
+    );
     return (
       <div className="App">
-        {this.state.monsters.map(monster => (
-          <h1 key={monster.id}> {monster.name}</h1>
-        ))}
+        <SearchBox
+          placeholder="Please type something here"
+          handleChange={this.handleChange}
+        />
+        <CardList monsters={filteredMonsters} />
       </div>
     );
   }
