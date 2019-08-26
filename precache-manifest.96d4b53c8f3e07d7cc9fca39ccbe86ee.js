7b5ec3730fc20c36c78c576c92b28125f802d868
Loic Masson <loic.masson@blueprintgenetics.com>
2019-08-26 11:42:18 +0300

    Updates

---
+++ precache-manifest.96d4b53c8f3e07d7cc9fca39ccbe86ee.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1739c9cdceb772a7371afbd91ddf4c0e",
+    "url": "/finnish-rolodex/index.html"
+  },
+  {
+    "revision": "a6f90c120f7106d3d0b5",
+    "url": "/finnish-rolodex/static/css/main.5ceecedb.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/finnish-rolodex/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "a6f90c120f7106d3d0b5",
+    "url": "/finnish-rolodex/static/js/main.0142f48d.chunk.js"
+  },
+  {
+    "revision": "ee39a81818d24ed88b95",
+    "url": "/finnish-rolodex/static/js/runtime~main.6a243718.js"
+  }
+]);
